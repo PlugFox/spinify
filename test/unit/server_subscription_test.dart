21e3d3dfdbbbde67a7df7c11641ec2cb8cbdb1e9
Plague Fox <plugfox@gmail.com>
2024-06-12 15:00:47 +0400

    Refactor SpinifyServerSubscription test to use fakeAsync

--- test/unit/server_subscription_test.dart
+++ test/unit/server_subscription_test.dart
@@ -5,76 +5,79 @@ import 'package:test/test.dart';
 void main() {
   group('SpinifyServerSubscription', () {
     test(
-        'Emulate server subscription',
-        () => fakeAsync((async) {
-              final client = Spinify(
-                config: SpinifyConfig(
-                  transportBuilder: $createFakeSpinifyTransport(
-                    overrideCommand: (command) => switch (command) {
-                      SpinifyConnectRequest request => SpinifyConnectResult(
+      'Emulate server subscription',
+      () => fakeAsync(
+        (async) {
+          final client = Spinify(
+            config: SpinifyConfig(
+              transportBuilder: $createFakeSpinifyTransport(
+                overrideCommand: (command) => switch (command) {
+                  SpinifyConnectRequest request => SpinifyConnectResult(
+                      id: request.id,
+                      timestamp: DateTime.now(),
+                      client: 'fake',
+                      version: '0.0.1',
+                      expires: false,
+                      ttl: null,
+                      data: null,
+                      subs: <String, SpinifySubscribeResult>{
+                        'notification:index': SpinifySubscribeResult(
                           id: request.id,
                           timestamp: DateTime.now(),
-                          client: 'fake',
-                          version: '0.0.1',
+                          data: const <int>[],
                           expires: false,
                           ttl: null,
-                          data: null,
-                          subs: <String, SpinifySubscribeResult>{
-                            'notification:index': SpinifySubscribeResult(
-                              id: request.id,
-                              timestamp: DateTime.now(),
+                          positioned: false,
+                          publications: <SpinifyPublication>[
+                            SpinifyPublication(
+                              channel: 'notification:index',
                               data: const <int>[],
-                              expires: false,
-                              ttl: null,
-                              positioned: false,
-                              publications: <SpinifyPublication>[
-                                SpinifyPublication(
-                                  channel: 'notification:index',
-                                  data: const <int>[],
-                                  info: SpinifyClientInfo(
-                                    client: 'fake',
-                                    user: 'fake',
-                                    channelInfo: const <int>[],
-                                    connectionInfo: const <int>[],
-                                  ),
-                                  timestamp: DateTime.now(),
-                                  tags: const <String, String>{
-                                    'type': 'notification',
-                                  },
-                                  offset: Int64.ZERO,
-                                ),
-                              ],
-                              recoverable: false,
-                              recovered: false,
-                              since: (epoch: '...', offset: Int64.ZERO),
-                              wasRecovering: false,
+                              info: SpinifyClientInfo(
+                                client: 'fake',
+                                user: 'fake',
+                                channelInfo: const <int>[],
+                                connectionInfo: const <int>[],
+                              ),
+                              timestamp: DateTime.now(),
+                              tags: const <String, String>{
+                                'type': 'notification',
+                              },
+                              offset: Int64.ZERO,
                             ),
-                          },
-                          pingInterval: const Duration(seconds: 25),
-                          sendPong: false,
-                          session: 'fake',
-                          node: 'fake',
+                          ],
+                          recoverable: false,
+                          recovered: false,
+                          since: (epoch: '...', offset: Int64.ZERO),
+                          wasRecovering: false,
                         ),
-                      _ => null,
-                    },
-                  ),
-                ),
-              )..connect('ws://localhost:8000/connection/websocket');
-              async.elapse(client.config.timeout);
-              expect(
-                client.subscriptions.server,
-                isA<Map<String, SpinifyServerSubscription>>()
-                    .having(
-                      (s) => s.length,
-                      'length',
-                      1,
-                    )
-                    .having(
-                      (s) => s['notification:index'],
-                      'notification:index',
-                      isA<SpinifyServerSubscription>(),
+                      },
+                      pingInterval: const Duration(seconds: 25),
+                      sendPong: false,
+                      session: 'fake',
+                      node: 'fake',
                     ),
-              );
-            }));
+                  _ => null,
+                },
+              ),
+            ),
+          )..connect('ws://localhost:8000/connection/websocket');
+          async.elapse(client.config.timeout);
+          expect(
+            client.subscriptions.server,
+            isA<Map<String, SpinifyServerSubscription>>()
+                .having(
+                  (s) => s.length,
+                  'length',
+                  1,
+                )
+                .having(
+                  (s) => s['notification:index'],
+                  'notification:index',
+                  isA<SpinifyServerSubscription>(),
+                ),
+          );
+        },
+      ),
+    );
   });
 }
