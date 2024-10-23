6051bb1ee93b7c8911dddb8806ad2edb9f0fd0c8
Plague Fox <plugfox@gmail.com>
2024-10-24 01:07:55 +0400

    Add mockito

--- test/unit/server_subscription_test.dart
+++ test/unit/server_subscription_test.dart
@@ -1,12 +1,32 @@
 import 'package:fake_async/fake_async.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:spinify/spinify.dart';
 import 'package:test/test.dart';
 
+import 'server_subscription_test.mocks.dart';
+
+@GenerateNiceMocks([MockSpec<WebSocket>(as: #MockWebSocket)])
 void main() {
   group('SpinifyServerSubscription', () {
     test(
       'Emulate server subscription',
       () => fakeAsync(
         (async) {
+          final ws = MockWebSocket();
+          when(ws.stream).thenAnswer(
+            (_) => Stream.fromIterable(
+              const <List<int>>[
+                <int>[0, 0, 0, 0, 0, 0, 0, 0],
+              ],
+            ),
+          );
+          when(ws.isClosed).thenReturn(false);
+          when(ws.add(any)).thenAnswer((_) {});
+          when(ws.close()).thenAnswer((_) {});
+          // TODO: Encode response data for mocks
+          // Mike Matiunin <plugfox@gmail.com>, 24 October 2024
+
           /* final client = Spinify(
             config: SpinifyConfig(
               transportBuilder: $createFakeSpinifyTransport(
