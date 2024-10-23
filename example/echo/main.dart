27e4f436c4ca9287380f1cadd610b7a049c439be
Plague Fox <plugfox@gmail.com>
2024-10-23 15:20:48 +0400

    Add example

--- example/echo/main.dart
+++ example/echo/main.dart
@@ -1,3 +1,19 @@
+// ignore_for_file: avoid_print
+
 import 'package:spinify/spinify.dart';
 
-void main() => Spinify();
+const url = 'ws://localhost:8000/connection/websocket';
+
+void main() {
+  final client = Spinify(
+    config: SpinifyConfig(
+      logger: (level, event, message, context) => print('[$event] $message'),
+    ),
+  );
+  var prev = client.state;
+  client.states.listen((next) {
+    print('$prev -> $next');
+    prev = next;
+  });
+  client.connect(url).ignore();
+}
