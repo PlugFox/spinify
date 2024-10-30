987c58b3f77c4831e407935bf45b6a5b17b6b01d
Plague Fox <plugfox@gmail.com>
2024-10-30 13:21:31 +0400

    Refactor SpinifyServerSubscription test to use a fake WebSocket and handle server subscription

--- test/unit/server_subscription_test.dart
+++ test/unit/server_subscription_test.dart
@@ -1,86 +1,93 @@
+import 'dart:async';
+
 import 'package:fake_async/fake_async.dart';
-import 'package:mockito/annotations.dart';
-import 'package:mockito/mockito.dart';
 import 'package:spinify/spinify.dart';
+import 'package:spinify/src/protobuf/client.pb.dart' as pb;
 import 'package:test/test.dart';
 
-import 'server_subscription_test.mocks.dart';
+import 'codecs.dart';
+import 'web_socket_fake.dart';
 
-@GenerateNiceMocks([MockSpec<WebSocket>(as: #MockWebSocket)])
+//import 'server_subscription_test.mocks.dart';
+
+//@GenerateNiceMocks([MockSpec<WebSocket>(as: #MockWebSocket)])
 void main() {
   group('SpinifyServerSubscription', () {
+    const url = 'ws://localhost:8000/connection/websocket';
+    final buffer = SpinifyLogBuffer(size: 10);
+
+    Spinify createFakeClient([Future<WebSocket> Function(String)? transport]) =>
+        Spinify(
+          config: SpinifyConfig(
+            transportBuilder: ({required url, headers, protocols}) =>
+                transport?.call(url) ?? Future.value(WebSocket$Fake()),
+            logger: buffer.add,
+          ),
+        );
+
     test(
-      'Emulate server subscription',
+      'Emulate_server_subscription',
       () => fakeAsync(
         (async) {
-          final ws = MockWebSocket();
-          when(ws.stream).thenAnswer(
-            (_) => Stream.fromIterable(
-              const <List<int>>[
-                <int>[0, 0, 0, 0, 0, 0, 0, 0],
-              ],
-            ),
-          );
-          when(ws.isClosed).thenReturn(false);
-          when(ws.add(any)).thenAnswer((_) {});
-          when(ws.close()).thenAnswer((_) {});
-          // TODO: Encode response data for mocks
-          // Mike Matiunin <plugfox@gmail.com>, 24 October 2024
-
-          /* final client = Spinify(
-            config: SpinifyConfig(
-              transportBuilder: $createFakeSpinifyTransport(
-                overrideCommand: (command) => switch (command) {
-                  SpinifyConnectRequest request => SpinifyConnectResult(
-                      id: request.id,
-                      timestamp: DateTime.now(),
-                      client: 'fake',
-                      version: '0.0.1',
-                      expires: false,
-                      ttl: null,
-                      data: null,
-                      subs: <String, SpinifySubscribeResult>{
-                        'notification:index': SpinifySubscribeResult(
-                          id: request.id,
-                          timestamp: DateTime.now(),
-                          data: const <int>[],
-                          expires: false,
-                          ttl: null,
-                          positioned: false,
-                          publications: <SpinifyPublication>[
-                            SpinifyPublication(
-                              channel: 'notification:index',
-                              data: const <int>[],
-                              info: SpinifyClientInfo(
-                                client: 'fake',
-                                user: 'fake',
-                                channelInfo: const <int>[],
-                                connectionInfo: const <int>[],
+          final client = createFakeClient(
+            (_) async => WebSocket$Fake()
+              ..onAdd = (bytes, sink) {
+                final command = ProtobufCodec.decode(pb.Command(), bytes);
+                scheduleMicrotask(() {
+                  if (command.hasConnect()) {
+                    sink.add(
+                      ProtobufCodec.encode(
+                        pb.Reply(
+                          id: command.id,
+                          connect: pb.ConnectResult(
+                            client: 'fake',
+                            version: '0.0.1',
+                            expires: false,
+                            ttl: null,
+                            data: null,
+                            subs: <String, pb.SubscribeResult>{
+                              'notification:index': pb.SubscribeResult(
+                                data: const <int>[],
+                                epoch: '...',
+                                offset: Int64.ZERO,
+                                expires: false,
+                                ttl: null,
+                                positioned: false,
+                                publications: <pb.Publication>[
+                                  pb.Publication(
+                                    data: const <int>[],
+                                    info: pb.ClientInfo(
+                                      client: 'fake',
+                                      user: 'fake',
+                                    ),
+                                    tags: const <String, String>{
+                                      'type': 'notification',
+                                    },
+                                  ),
+                                ],
+                                recoverable: false,
+                                recovered: false,
+                                wasRecovering: false,
                               ),
-                              timestamp: DateTime.now(),
-                              tags: const <String, String>{
-                                'type': 'notification',
-                              },
-                              offset: Int64.ZERO,
-                            ),
-                          ],
-                          recoverable: false,
-                          recovered: false,
-                          since: (epoch: '...', offset: Int64.ZERO),
-                          wasRecovering: false,
+                            },
+                            ping: 600,
+                            pong: false,
+                            session: 'fake',
+                            node: 'fake',
+                          ),
                         ),
-                      },
-                      pingInterval: const Duration(seconds: 25),
-                      sendPong: false,
-                      session: 'fake',
-                      node: 'fake',
-                    ),
-                  _ => null,
-                },
-              ),
-            ),
-          )..connect('ws://localhost:8000/connection/websocket');
+                      ),
+                    );
+                  }
+                });
+              },
+          );
+
+          client.connect(url).ignore();
           async.elapse(client.config.timeout);
+          expect(client.state.isConnected, isTrue);
+          expect(client.subscriptions.server, isNotEmpty);
+          expect(client.subscriptions.server['notification:index'], isNotNull);
           expect(
             client.subscriptions.server,
             isA<Map<String, SpinifyServerSubscription>>()
@@ -94,7 +101,9 @@ void main() {
                   'notification:index',
                   isA<SpinifyServerSubscription>(),
                 ),
-          ); */
+          );
+
+          client.close();
         },
       ),
     );
